e9f2c6ff7fbc5431e0f4139cda3728050f960e2b
diubaldo94 <diubaldo94@gmail.com>
2020-05-30 01:19:22 +0200

    written service unit tests

---
+++ SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/DataGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesTaxesCalculation.UnitTests
+{
+    public class DataGenerator
+    {
+        public static (PurchaseContainer, ReceiptContainer) GetSamplePurchaseContainerWithRelatedReceipt()
+        {
+            var purchaseRow1 = new PurchaseRow(new Item("book", 12.49), 2, false);
+            var purchaseRow2 = new PurchaseRow(new Item("music CD", 14.99), 1, false);
+            var purchaseRow3 = new PurchaseRow(new Item("chocolate bar", 0.85), 1, false);
+            var purchaseRow4 = new PurchaseRow(new Item("box of chocolates", 10), 1, false);
+            var purchaseRow5 = new PurchaseRow(new Item("bottle of perfume", 47.50), 1, false);
+
+            var taxesConfigNoImportDuty = new TaxesConfig(0.1, 0);
+            var taxesConfigImportDuty5 = new TaxesConfig(0.1, 0.05);
+
+            var purchases = new List<Purchase>()
+            {
+                new Purchase(new List<PurchaseRow>{purchaseRow1, purchaseRow2, purchaseRow3}),
+                new Purchase(new List<PurchaseRow>{purchaseRow4, purchaseRow5}),
+            };
+            var receipts = new List<Receipt>()
+            {
+                new Receipt(
+                    new List<ReceiptRow>
+                    {
+                        new ReceiptRow(purchaseRow1),
+                        new ReceiptRow(purchaseRow2),
+                        new ReceiptRow(purchaseRow3),
+                    }
+                ),
+                new Receipt(
+                    new List<ReceiptRow>
+                    {
+                        new ReceiptRow(purchaseRow4),
+                        new ReceiptRow(purchaseRow5),
+                    }
+                ),
+            };
+            return (new PurchaseContainer(purchases), new ReceiptContainer(receipts));
+        }
+    }
+}
--- SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/SalesTaxesServiceTest.cs
+++ SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/SalesTaxesServiceTest.cs
@@ -25,6 +25,8 @@ namespace SalesTaxesCalculation.UnitTests
 
             _logHandler = new Mock<ILogHandler>();
 
+            //todo make an uow inside to process file batch in transaction
+            //todo how to handle file management and transaction?
             _sut = new SalesTaxesService(_purchaseRepositoryMock.Object, _taxesCalculatorMock.Object, _receiptNotifier.Object);
         }
 
@@ -42,6 +44,7 @@ namespace SalesTaxesCalculation.UnitTests
             await _sut.ProcessPurchases();
             
             _receiptNotifier.Verify(i => i.Notify(It.Is<ReceiptContainer>(p => p.Equals(sampleReceipts))), Times.Once);
+            _purchaseRepositoryMock.Verify(i => i.Restore(It.IsAny<PurchaseContainer>(), Times.Never));
         }
 
         [Fact]
@@ -54,6 +57,7 @@ namespace SalesTaxesCalculation.UnitTests
 
             _taxesCalculatorMock.Setup(i => i.Compute(It.IsAny<PurchaseContainer>())).Verify(Times.Never);
             _receiptNotifier.Verify(i => i.Notify(It.IsAny<ReceiptContainer>()), Times.Never);
+            _purchaseRepositoryMock.Verify(i => i.Restore(It.IsAny<PurchaseContainer>(), Times.Never));
         }
 
         [Fact]
@@ -68,24 +72,11 @@ namespace SalesTaxesCalculation.UnitTests
             _logHandler.Verify(i => i.LogError(expectedErrMessage), Times.Once);
             _taxesCalculatorMock.Setup(i => i.Compute(It.IsAny<PurchaseContainer>())).Verify(Times.Never);
             _receiptNotifier.Verify(i => i.Notify(It.IsAny<ReceiptContainer>()), Times.Never);
+            _purchaseRepositoryMock.Verify(i => i.Restore(It.IsAny<PurchaseContainer>(), Times.Never));
         }
 
         [Fact]
         public void Should_Throws_IfCalculatorFailToProcessPurchases()
-        {
-            var expectedErrMessage = "error on process data";
-            var containerWithNoPurchases = new PurchaseContainer(new List<Purchase>());
-            _purchaseRepositoryMock.Setup(i => i.GetPurchases()).ThrowsAsync(new Exception(expectedErrMessage)).Verifiable();
-
-            Assert.ThrowsAsync<Exception>(async () => await _sut.ProcessPurchases());
-
-            _logHandler.Verify(i => i.LogError(expectedErrMessage), Times.Once);
-            _taxesCalculatorMock.Setup(i => i.Compute(It.IsAny<PurchaseContainer>())).Verify(Times.Never);
-            _receiptNotifier.Verify(i => i.Notify(It.IsAny<ReceiptContainer>()), Times.Never);
-        }
-
-        [Fact]
-        public void Should_Throws_IfNotifierFailToProcessAnyReceipt()
         {
             var expectedErrMsg = "error on process data";
             var samplePurchases = GetSamplePurchaseList();
@@ -99,29 +90,30 @@ namespace SalesTaxesCalculation.UnitTests
             Assert.ThrowsAsync<Exception>(async () => await _sut.ProcessPurchases());
 
             _logHandler.Verify(i => i.LogError(expectedErrMessage), Times.Once);
-            _purchaseRepositoryMock.Restore(samplePurchases.BatchIdentifier);
-            _taxesCalculatorMock.Setup(i => i.Compute(It.IsAny<PurchaseContainer>())).Verify(Times.Exactly(samplePurchases.List.Count()));
+            _taxesCalculatorMock.Setup(i => i.Compute(It.IsAny<PurchaseContainer>())).Verify(Times.Never);
             _receiptNotifier.Verify(i => i.Notify(It.IsAny<ReceiptContainer>()), Times.Never);
+            _purchaseRepositoryMock.Verify(i => i.Restore(It.Is<PurchaseContainer>(p => p.Equals(samplePurchases)), Times.Once));
         }
 
-        private PurchaseContainer GetSamplePurchaseList()
+        [Fact]
+        public void Should_Throws_IfNotifierFailToProcessAnyReceipt()
         {
-            var purchases = new List<Purchase>()
-            {
-                new Purchase(),
-                new Purchase(),
-            };
-            return new PurchaseContainer(purchases);
+            var expectedErrMsg = "error on notify data";
+            var samplePurchases = GetSamplePurchaseList();
+            var sampleReceipts = GetSampleReceiptList();
+            _purchaseRepositoryMock.Setup(i => i.GetPurchases()).ReturnsAsync(samplePurchases).Verifiable();
+            _taxesCalculatorMock.Setup(i => i.Compute(It.Is<Purchase>(p => p.Equals(samplePurchases.List[0]))))
+                .Returns(sampleReceipts.List[0]).Verifiable();
+            _taxesCalculatorMock.Setup(i => i.Compute(It.Is<Purchase>(p => p.Equals(samplePurchases.List[1]))))
+                .Returns(sampleReceipts.List[1]).Verifiable();
+            _receiptNotifier.Setup(i => i.Notify(It.IsAny<ReceiptContainer>())).ThrowsAsync(new Exception(expectedErrMsg));
+
+            Assert.ThrowsAsync<Exception>(async () => await _sut.ProcessPurchases());
+
+            _logHandler.Verify(i => i.LogError(expectedErrMessage), Times.Once);
+            _purchaseRepositoryMock.Verify(i => i.Restore(It.Is<PurchaseContainer>(p => p.Equals(samplePurchases)), Times.Once));
         }
 
-        private ReceiptContainer GetSampleReceiptList()
-        {
-            var receipts = new List<Receipt>()
-            {
-                new Receipt(),
-                new Receipt(),
-            };
-            return new ReceiptContainer(receipts);
-        }
+        
     }
 }
