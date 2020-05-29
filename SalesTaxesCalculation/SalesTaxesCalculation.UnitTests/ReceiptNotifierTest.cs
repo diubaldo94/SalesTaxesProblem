c833efd0aa7313651ae90c9a9fd93777e9c00f9e
diubaldo94 <diubaldo94@gmail.com>
2020-05-30 01:19:47 +0200

    added notfiier and calculator unit tests classes

---
+++ SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/ReceiptNotifierTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesTaxesCalculation.UnitTests
+{
+    class ReceiptNotifierTest
+    {
+        private readonly ILogHandler _logHandlerMock;
+        private readonly ReceiptNotifier _sut;
+
+        public ReceiptNotifierTest()
+        {
+            _logHandlerMock = new Mock<ILogHandler>();
+            _sut = new ReceiptNotifier(_logHandlerMock.Object);
+        }
+
+
+    }
+}
---
+++ SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/SalesTaxesCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesTaxesCalculation.UnitTests
+{
+    class SalesTaxesCalculatorTest
+    {
+        public SalesTaxesCalculatorTest()
+        {
+            //todo use chain of factory for that
+        }
+    }
+}
