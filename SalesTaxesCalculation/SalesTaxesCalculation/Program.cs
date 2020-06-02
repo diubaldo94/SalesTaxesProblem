b2b3eb7ecb227a0bc4c7b4150814b80a96ceae3c
diubaldo94 <diubaldo94@gmail.com>
2020-06-03 00:31:28 +0200

    implemented startup with ioc

--- SalesTaxesCalculation/SalesTaxesCalculation/Program.cs
+++ SalesTaxesCalculation/SalesTaxesCalculation/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SalesTaxesCalculation.Core;
+using SalesTaxesCalculation.UnitTests;
 
 namespace SalesTaxesCalculation
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((ctx, services)  =>
+                {
+                    var config = new ConfigurationBuilder();
+
+                    //services.Configure<AppSettings>(ctx.Configuration.GetSection("ExemptTypes"));
+
+                    //todo to inject
+                    //services.Configure(new RepositoryConfig(ctx.Configuration.GetSection("RepositoryConfig")));
+
+                    services.AddSingleton(typeof(IRepository<>), typeof(FileRepository<>));
+                    services.AddSingleton<IMapper<PurchaseContainer>, CustomMapper>();
+                    services.AddSingleton<ILogHandler, LogHandler>();
+                    services.AddSingleton<INotifier<ReceiptContainer>, ReceiptNotifier>();
+                    services.AddSingleton<SalesTaxesService>();
+                    //todo to inject from config
+                    services.AddSingleton<ICalculator<Purchase, IReceipt>>(
+                        new SalesTaxesCalculator(new TaxesConfiguration(0.1, "BASIC", 0.05, "IMPORT"), 
+                        new BasicTaxExemptTypes(new List<string> { "food", "medical products", "books" })));
+                    services.AddHostedService<Service>();
+                });
     }
 }
