52962ec54cbfebd48efded5b21ef8952c751e7ed
diubaldo94 <diubaldo94@gmail.com>
2020-06-03 20:19:46 +0200

    set differente color on app console logging

--- SalesTaxesCalculation/SalesTaxesCalculation.Application/LogHandler.cs
+++ SalesTaxesCalculation/SalesTaxesCalculation.Application/LogHandler.cs
@@ -7,11 +7,13 @@ namespace SalesTaxesCalculation.Application
     {
         public void LogError(string errMessage)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(errMessage);
         }
 
         public void LogInfo(string logMessage)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(logMessage);
         }
     }
