f3a31c19a575197017f847fa54d504284c45de61
diubaldo94 <diubaldo94@gmail.com>
2020-06-03 22:24:26 +0200

    added new command and message generator on notifier

--- SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/ReceiptNotifierTest.cs
+++ SalesTaxesCalculation/SalesTaxesCalculation.UnitTests/ReceiptNotifierTest.cs
@@ -11,23 +11,31 @@ namespace SalesTaxesCalculation.UnitTests
     public class ReceiptNotifierTest
     {
         private readonly Mock<ILogHandler> _logHandlerMock;
+        private readonly Mock<ICommand> _command1Mock;
+        private readonly Mock<IMessageGenerator<ReceiptContainer>> _messageGenerator;
         private readonly ReceiptNotifier _sut;
 
         public ReceiptNotifierTest()
         {
             _logHandlerMock = new Mock<ILogHandler>();
-
-            _sut = new ReceiptNotifier(_logHandlerMock.Object);
+            _command1Mock = new Mock<ICommand>();
+            _messageGenerator = new Mock<IMessageGenerator<ReceiptContainer>>();
+            _sut = new ReceiptNotifier(_logHandlerMock.Object, _command1Mock.Object, _messageGenerator.Object);
         }
 
         [Fact]
         public async Task Should_ShowOnConsoleResult()
         {
             var receipts = DataGenerator.GetReceipts();
-            await _sut.Notify(new ReceiptContainer(receipts));
+            var receiptsContainer = new ReceiptContainer(receipts);
 
             var expectedLogMessage = DataGenerator.GetMessage().Trim();
+            _messageGenerator.Setup(i => i.Generate(receiptsContainer)).Returns(expectedLogMessage).Verifiable();
+
+            await _sut.Notify(receiptsContainer);
+
             _logHandlerMock.Verify(i => i.LogInfo(expectedLogMessage), Times.Once);
+            _command1Mock.Verify(i => i.Launch(expectedLogMessage), Times.Once);
         }
     }
 }
