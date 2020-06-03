5f318abdab46b95e26df65b58ff2ced94c3a8c91
diubaldo94 <diubaldo94@gmail.com>
2020-06-03 23:50:26 +0200

    modified scripts

--- ExecuteWithSampleFile.bat
+++
@@ -1,8 +0,0 @@
-if not exist "workdir\Input" mkdir workdir\Input
-if not exist "workdir\Output" mkdir workdir\Output
-if not exist "workdir\Error" mkdir workdir\Error
-if not exist "workdir\Backup" mkdir workdir\Backup
-xcopy SalesTaxesCalculation\SalesTaxesCalculation.Application\Sample\samplePurchase.json workdir\Input /Y
-cd SalesTaxesCalculation/SalesTaxesCalculation.Application
-dotnet run --project SalesTaxesCalculation.Application.csproj
-pause
